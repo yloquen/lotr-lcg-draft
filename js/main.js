351e65476eeff09ab06d9fa7fd8a87099733166c
I.Kostov <i.kostov@wienergames.com>
2018-09-21 00:26:17 +0300

    prog

--- js/main.js
+++ js/main.js
@@ -4,8 +4,34 @@ let cardsSelected = [];
 
 function start()
 {
-    selectorFunctions[0].func();
+    let heroesAvailable = selectorFunctions[0].func();
+    populateSelector(heroesAvailable);
 }
+
+
+function populateSelector(cardsAvailable)
+{
+    let selector = document.getElementById("selector");
+
+    for (let cardIdx=0; cardIdx < cardsAvailable.length; cardIdx++)
+    {
+        let cardData = cardsAvailable[cardIdx];
+        let img = document.createElement("img");
+        img.src = "https://digital.ringsdb.com" + cardData.imagesrc;
+        img.classList.add("selectable_card");
+        img.cardData = cardData;
+        img.onclick = addCard;
+        selector.appendChild(img);
+    }
+
+}
+
+
+function addCard(e)
+{
+    let cardData = e.currentTarget.cardData;
+}
+
 
 
 selectorFunctions =
@@ -16,13 +42,15 @@ selectorFunctions =
             let allHeroes = cardsData.filter(cardData => cardData.type_code === "hero");
             allHeroes = JSON.parse(JSON.stringify(allHeroes));
 
-            let pickupHeroes = [];
+            let heroesAvailable = [];
             do
             {
                 let rndIdx = Math.floor(Math.random() * allHeroes.length);
-                pickupHeroes.push(allHeroes.splice(rndIdx, 1));
+                heroesAvailable.push(allHeroes.splice(rndIdx, 1)[0]);
             }
-            while(pickupHeroes.length < 2);
+            while(heroesAvailable.length < 2);
+
+            return heroesAvailable;
         }
     }
 ];
